c3debbacd7494b945bb7edccd9164731fff23145
charan1435 <sc16101@gmail.com>
2024-11-25 16:45:19 +0530

    Initial Commit

---
+++ ModelViewControllerImp/ModelViewControllerImpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewControllerImpApp.swift
+//  ModelViewControllerImp
+//
+//  Created by Sabith Salman on 2024-11-25.
+//
+
+import SwiftUI
+
+@main
+struct ModelViewControllerImpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
