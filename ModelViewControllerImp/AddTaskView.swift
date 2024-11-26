1c9e6838941c411158234c49cc950b94f6588346
charan1435 <sc16101@gmail.com>
2024-11-26 14:12:21 +0530

    Full Logic of Model, Controller and  a add view Tsk

---
+++ ModelViewControllerImp/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  ModelViewControllerImp
+//
+//  Created by Sabith Salman on 2024-11-25.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
---
+++ ModelViewControllerImp/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  ModelViewControllerImp
+//
+//  Created by Sabith Salman on 2024-11-25.
+//
+
+import Foundation
